03267a587301a996b51acdbb1be0cee2279502a2
Tom Richards <tom@tomrichards.net>
2020-07-07 18:28:41 -0400

    Revert to make install

--- ext/dualcone/extconf.rb
+++ ext/dualcone/extconf.rb
@@ -24,7 +24,7 @@ append_cflags(cflags)
 Dir.chdir(LIBHYDROGEN_DIR) do
   system('make clean')
   system("export CFLAGS='#{cflags.join(' ')}'; make")
-  system('make lib')
+  system('PREFIX=. make install')
 
   # Ensure that our bundled version of libhydrogen is always used
   $DEFLIBPATH.unshift("#{LIBHYDROGEN_DIR}/lib")
