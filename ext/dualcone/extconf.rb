e6322ebf6630876c6dc345931698857c3ad7469e
Tom Richards <tom@tomrichards.net>
2021-03-09 11:53:13 -0500

    Replace constant

--- ext/dualcone/extconf.rb
+++ ext/dualcone/extconf.rb
@@ -16,21 +16,21 @@ else
   cflags.unshift('-Os')
 end
 
-LIBHYDROGEN_DIR = File.join(__dir__, '..', '..', 'vendor', 'libhydrogen')
+libhydrogen_dir = File.join(__dir__, '..', '..', 'vendor', 'libhydrogen')
 
 abort 'ERROR: make is required to build libhydrogen.' unless find_executable('make')
 
 append_cflags(cflags)
 
 # Build the bundled version of libhydrogen in vendor
-Dir.chdir(LIBHYDROGEN_DIR) do
+Dir.chdir(libhydrogen_dir) do
   system('make clean')
   system("export CFLAGS='#{cflags.join(' ')}'; make")
   system('PREFIX=. make install')
 
   # Ensure that our bundled version of libhydrogen is always used
-  $DEFLIBPATH.unshift("#{LIBHYDROGEN_DIR}/lib")
-  dir_config('hydrogen', "#{LIBHYDROGEN_DIR}/include", "#{LIBHYDROGEN_DIR}/lib")
+  $DEFLIBPATH.unshift("#{libhydrogen_dir}/lib")
+  dir_config('hydrogen', "#{libhydrogen_dir}/include", "#{libhydrogen_dir}/lib")
 end
 
 abort 'ERROR: Failed to build libhydrogen.' unless have_library('hydrogen') && have_header('hydrogen.h')
